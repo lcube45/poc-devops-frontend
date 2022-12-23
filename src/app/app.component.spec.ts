ac64fd24c92a7c61a02b5e69ae095856e31e65d5
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-12-23 10:21:39 +0100

    demo a fault test and code smell

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontendxxx');
+    expect(app.title).toEqual('frontendyyy');
   });
 
   it('should render title', () => {

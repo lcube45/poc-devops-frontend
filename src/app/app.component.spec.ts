9cd2f89b3af501a06d4635f56411dd2ac80e61ec
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-12-23 17:40:50 +0100

    debug deploy workflow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'hello-world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('hello-world');
   });
 
   it('should render title', () => {
